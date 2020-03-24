9fe07e497781bbbc9d448fc1707c0491921e9ae8
abeata <62559783+abeata@users.noreply.github.com>
2020-03-24 14:40:38 +0100

    WERK

--- commands/clear.js
+++ commands/clear.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    if (!message.member.haspermissions("MANAGE_MESSAGES")) return message.reply("You dont have permission");
+    if (!message.member.hasPermissions("MANAGE_MESSAGES")) return message.reply("You dont have permission");
 
     if (!args[0]) return message.reply("Give a number!");
 
@@ -11,6 +11,11 @@ module.exports.run = async (bot, message, args) => {
         var amount = ParseInt(args[0]) + 1;
 
         message.channel.bulkDelete(amount).then(() => {
+            if(args[0] == 0){
+                message.channel.send("cant delete 0 messages").then(msg => msg.delete(3000));
+            } else if (args[0] == 1){
+                message.channel.send(`you have ${args[0]} message deleted`).then(msg => msg.delete(3000));
+            }
 
             message.channel.send(`You have ${args[0]} messages deleted`).then(msg => msg.delete(3000));
 
