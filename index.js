70a2aee1adbd06b70d76dde88c8ae32a4b0718ab
abeata <62559783+abeata@users.noreply.github.com>
2020-03-24 12:27:54 +0100

    er

--- index.js
+++ index.js
@@ -6,11 +6,33 @@ const bot = new discord.Client();
 
 bot.on("ready", async () => {
 
-    console.log('${bot.user.username} is online');
+    console.log('LightMc bot is online!');
 
-    bot.user.setActivity("LightMc", {type: "PLAYING"});
+    bot.user.setActivity("LightMc", { type: "PLAYING" });
 
-})
+});
+
+bot.on("message", async message => {
+
+    if (message.author.bot) return;
+
+    if (message.channel.type === "dm") return;
+
+    var prefix = botConfig.prefix;
+
+    var messageArray = message.content.split(" ");
+
+    var command = messageArray[0];
+
+    var arguments = messageArray.slice(1);
+
+    if (command === '${prefix}hallo') {
+
+        return message.channel.send("Hallo")
+
+    }
+
+});
 
 
 bot.login(process.env.token);
