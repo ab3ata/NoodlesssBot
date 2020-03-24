7536c78ac3c1cd550e201c15208a68884ef61d1f
abeata <62559783+abeata@users.noreply.github.com>
2020-03-24 12:11:22 +0100

    effe nieuw van scratch

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
-const Discord = require('discord.js');
-const botConfig = require("./botconfig.json")
+const discord = require("discord.js");
+const botConfig = require("./botconfig.json");
 
-const bot = new Discord.Client();
-bot.commands = new Discord.Collection();
+const bot = new discord.Client();
 
-const fs = require("fs");
 
-fs.readdir("./commands/" , (err, files) => {
-
-    if(err) console.log(err);
-
-    var jsFiles = files.filter(f => f.split(".").pop() === "js");
-
-    if(jsFiles.length <=0) {
-        console.log("kan geen files vinden");
-        return;
-    }
-
-    jsFiles.array.forEach(element => {
-        
-        var fileGet = require('.commands/$(f)');
-        console.log("de file $(f) is geladen");
-
-        bot.commands.set(fileGet.help.name, fileGet);
-    });
-})
 bot.on("ready", async () => {
-    console.log('${bot.user.username} is online!')
-})
-bot.on("message", async message => {
-    var commands = bot.commands.get(command.slice(prefix.length));
 
-    if(commands) commands.run(bot, message, args);
+    console.log('${bot.user.username} is online')
+
 })
+
+
 bot.login(process.env.token);
