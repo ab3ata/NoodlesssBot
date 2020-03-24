30fbc832e02d056a7856c20fd9b79493f4220498
abeata <62559783+abeata@users.noreply.github.com>
2020-03-24 16:00:43 +0100

    Update staffapply.js

--- commands/staffapply.js
+++ commands/staffapply.js
@@ -80,15 +80,19 @@ module.exports.run = async (bot, message, args) => {
                kanaal.send(applications).then(messageReaction => {
                 messageReaction.react("👍");
                 messageReaction.react("👎");
-            })
-            const filter = (reaction, user) => {
-                return reaction.emoji.name === '👍' && user.id === message.author.id;
-            };
-            
-            message.awaitReactions(filter, { max: 4, time: 10000, errors: ['time'] })
-                .then(collected => console.log(collected.size))
-                .catch(collected => {
-                    console.log(`After a minute, only ${collected.size} out of 4 reacted.`);
+                const filter = (reaction, user) => {
+                    return reaction.emoji.name === '👍' && user.id === message.author.id;
+                };
+                
+                const collector = message.createReactionCollector(filter, { time: 15000 });
+                
+                collector.on('collect', (reaction, reactionCollector) => {
+                    console.log(`Collected ${reaction.emoji.name}`);
+                });
+                
+                collector.on('end', collected => {
+                    console.log(`Collected ${collected.size} items`);
+                });
                 });
               })
           })
