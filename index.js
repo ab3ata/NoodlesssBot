38ba8bcb3b7021d312aaa1bc920d1eafe46e27e0
abeata <62559783+abeata@users.noreply.github.com>
2020-03-24 13:34:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -58,10 +58,43 @@ bot.on("message", async message => {
             .addField('bot naam', bot.user.username)
             .addField("gemaakt op", bot.user.createdAt);
 
+
+
+
             return message.channel.send(serverEmbed);
 
     }
 
+    if (command === `${prefix}kick`) {
+
+
+        var kickUser = message.guild.member(message.mentions.users.first() || message.guild.members(arguments[0]));
+
+        if(!kickUser) return message.channel.send("User not found");
+
+        var reason = arguments.join(" ").slice(22);
+
+        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("srry you cant do this!");
+
+        if(kickUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("you cant kick this player");
+
+        var kick = new discord.MessageEmbed()
+        .setDescription("kick")
+        .setColor("#8c6607")
+        .addField("Kicked User", kickUser)
+        .addField("Kicked By", message.author)
+        .addField("reason", reason);
+
+        var kickChannel = message.guild.channel.find("name", "commands");
+        if(!kickChannel) return message.guild.send("Cant find the channel!");
+
+        message.guild.member(kickUser).kick(reason);
+
+        kickChannel.send(kick);
+
+        return;
+    }
+    
 });
 
 
