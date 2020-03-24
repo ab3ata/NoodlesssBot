e54ec40287f6603d7dccde0664ca41b105b0a826
abeata <62559783+abeata@users.noreply.github.com>
2020-03-24 18:27:57 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -2,12 +2,11 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    // Create a reaction collector
+message.react("👍");
 const filter = (reaction, user) => reaction.emoji.name === '👌'
 const collector = message.createReactionCollector(filter, { time: 15000 });
 collector.on('collect', r => console.log(`Collected ${r.emoji.name}`));
 collector.on('end', collected => console.log(`Collected ${collected.size} items`));
-message.react("👍");
 }
 module.exports.help = {
     name: "test"
