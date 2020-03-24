4b74d4435419ca108824accc547f90dba22ffb54
abeata <62559783+abeata@users.noreply.github.com>
2020-03-24 10:36:19 +0100

    sdfs
    sd

--- index.js
+++ index.js
@@ -1,113 +1,36 @@
 const Discord = require('discord.js');
+const botConfig = require("./botconfig.json")
+
 const bot = new Discord.Client();
-const PREFIX = '!'
+bot.commands = new Discord.Collection();
 
-bot.on('ready', () =>{
-    console.log('this bot is ready!')
-});
+const fs = require("fs");
 
-bot.on('message', message=>{
-    let args = message.content.substring(PREFIX.length).split(" ");
-    switch(args[0]){
-        case 'help':
-                    const help = new Discord.MessageEmbed()
-	                .setColor('0xFFC300')
-	                .setTitle('New staff application!')
-                    .addFields(
-                        { name: '!staffapply', value: 'do !staffapply to apply for staff' }
-                         )
-                         channel.send(help); 
-    break;
+fs.readdir("./commands/" , (err, Files) => {
+
+    if(err) console.log(err);
+
+    var jsFiles = files.filter(f => f.split(".").pop() === "js")
+
+    if(jsFiles.length <=0) {
+        console.log("kan geen files vinden");
+        return;
     }
-    })
-bot.on('message', message=>{
-    let args = message.content.substring(PREFIX.length).split(" ");
-    switch(args[0]){
-        case 'staffapply':
-              const filter = m => m.author.id === message.author.id;
-              message.channel.send({embed: {
-                color: 0xFFC300,
-                description: "minecraft name:"
-              }})
-              message.channel.awaitMessages(filter, {max: 1}).then(collected => {
-                message.channel.send({embed: {
-                    color: 0xFFC300,
-                    description: "real name: (not needed)"
-                }})
-                message.channel.awaitMessages(filter, {max: 1}).then(collected2 => {
-                    message.channel.send({embed: {
-                        color: 0xFFC300,
-                        description: "age:"
-                    }})
-                    message.channel.awaitMessages(filter, {max: 1}).then(collected3 => {
-                        message.channel.send({embed: {
-                            color: 0xFFC300,
-                            description: "why do you want to be staff?"
-                        }})
-                        message.channel.awaitMessages(filter, {max: 1}).then(collected4 => {
-                            message.channel.send({embed: {
-                                color: 0xFFC300,
-                                description: "have you ever been staff before?"
-                            }})
-                            message.channel.awaitMessages(filter, {max: 1}).then(collected5 => {
-                                message.channel.send({embed: {
-                                    color: 0xFFC300,
-                                    description: "howlong have you been playing on the server?"
-                                }})
-                                message.channel.awaitMessages(filter, {max: 1}).then(collected6 => {
-                                    message.channel.send({embed: {
-                                        color: 0xFFC300,
-                                        description: "Have you ever been banned?"
-                                    }})
-                                    message.channel.awaitMessages(filter, {max: 1}).then(collected7 => {
-                                        message.channel.send({embed: {
-                                            color: 0xFFC300,
-                                            description: "if you would see anyone hacking what would you do? ban them permanent | ban them temp | just warn him and tell him to stop"
-                                        }})
-                                        message.channel.awaitMessages(filter, {max: 1}).then(collected8 => {
-                                            message.channel.send({embed: {
-                                                color: 0xFFC300,
-                                                description: "if you would see your friend hacking what would you do? ban them permanent | ban them temp | just warn him and tell him to stop"
-                                            }})
-                        
-                    message.channel.awaitMessages(filter, {max: 1}).then(collected9 => {
-                    let username = collected.first().content
-                    let username2 = collected2.first().content
-                    let username3 = collected3.first().content
-                    let username4 = collected4.first().content
-                    let username5 = collected5.first().content
-                    let username6 = collected6.first().content
-                    let username7 = collected7.first().content
-                    let username8 = collected8.first().content
-                    let username9 = collected9.first().content
-                    const applications = new Discord.MessageEmbed()
-	                .setColor('0xFFC300')
-	                .setTitle('New staff application!')
-                    .addFields(
-                        { name: 'minecraft name:', value: username },
-                        { name: 'real name: (not needed)', value: username2 },
-                        { name: 'age:', value: username3 },
-                        { name: 'why do you wanna be staff?', value: username4 },
-                        { name: 'have you ever been staff before?', value: username5 },
-                        { name: 'Have you ever been banned?', value: username6 },
-                        { name: 'howlong have you been playing on the server?', value: username7 },
-                        { name: 'if you would see anyone hacking what would you do? ban them permanent | ban them temp | just warn him and tell him to stop', value: username8 },
-                        { name: 'if you would see your friend hacking what would you do? ban them permanent | ban them temp | just warn him and tell him to stop', value: username9 }
-                         )
-                         var kanaal = message.guild.channels.cache.find(c => c.name == "application-log");
-                         kanaal.send(applications); 
-            
-                        })
-                    })
-                })
-            })
-        })
-    })
-})
-})
-})
-    break;
-    }
-    })
 
-        bot.login(process.env.token);
+    jsFiles.array.forEach(element => {
+        
+        var fileGet = require('.commands/$(f)');
+        console.log("de file $(f) is geladen");
+
+        bot.commands.set(fileGet.help.name, fileGet);
+    });
+})
+bot.on("ready", async () => {
+    console.log('${bot.user.username} is online!')
+})
+bot.on("message", async message => {
+    var commands = bot.commands.get(command.slice(prefix.length));
+
+    if(commands) command.run(bot, message, args);
+})
+bot.login(process.env.token);
