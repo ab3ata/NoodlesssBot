a0c1f2eb2abb6b02e861c4bc4549750af231b525
abeata <62559783+abeata@users.noreply.github.com>
2020-03-24 13:40:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -61,6 +61,8 @@ bot.on("message", async message => {
 
 
 
+
+
             return message.channel.send(serverEmbed);
 
     }
@@ -91,6 +93,35 @@ bot.on("message", async message => {
         message.guild.member(kickUser).kick(reason);
 
         kickChannel.send(kick);
+
+        return;
+    }
+
+    if (command === `${prefix}ban`) {
+
+        var banUser = message.guild.member(message.mentions.users.first() || message.guild.members(arguments[0]));
+
+        if(!banUser) return message.channel.send("User not found");
+
+        var reason = arguments.join(" ").slice(22);
+
+        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("srry you cant do this!");
+
+        if(banUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("you cant ban this player");
+
+        var ban = new discord.MessageEmbed()
+        .setDescription("ban")
+        .setColor("#8c6607")
+        .addField("banned User", banUser)
+        .addField("banned By", message.author)
+        .addField("reason", reason);
+
+        var banChannel = message.guild.channel.find("name", "commands");
+        if(!banChannel) return message.guild.send("Cant find the channel!");
+
+        message.guild.member(banUser).ban(reason);
+
+        banChannel.send(ban);
 
         return;
     }
