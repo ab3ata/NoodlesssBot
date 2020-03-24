b19bd5813dd95684f01b7dbea13a6e7c5499453d
abeata <62559783+abeata@users.noreply.github.com>
2020-03-24 15:57:09 +0100

    Update staffapply.js

--- commands/staffapply.js
+++ commands/staffapply.js
@@ -85,7 +85,7 @@ module.exports.run = async (bot, message, args) => {
                 return reaction.emoji.name === '👍' && user.id === message.author.id;
             };
             
-            message.awaitReactions(filter, { max: 4, time: 60000, errors: ['time'] })
+            message.awaitReactions(filter, { max: 4, time: 10000, errors: ['time'] })
                 .then(collected => console.log(collected.size))
                 .catch(collected => {
                     console.log(`After a minute, only ${collected.size} out of 4 reacted.`);
