8b2882e560c01107c41ee705b6b3d4c89f8b64cd
abeata <62559783+abeata@users.noreply.github.com>
2020-03-25 14:46:02 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,7 +1,7 @@
 const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    const oof = new Discord.MessageEmbed()
+    let oof = new Discord.MessageEmbed()
         .setTitle('oof');
 
     message.channel.send(embed).then(msg => {
