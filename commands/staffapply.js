f2aa34ac38c894e983f472d2a68796eae50c6931
abeata <62559783+abeata@users.noreply.github.com>
2020-03-25 13:05:08 +0100

    Update staffapply.js

--- commands/staffapply.js
+++ commands/staffapply.js
@@ -113,26 +113,9 @@ module.exports.run = async (bot, message, args) => {
             { name: 'test', value: username }
         )
     var kanaal = message.guild.channels.cache.find(c => c.name == "application-log");
-    kanaal.send(Acceptordecline);
-    message.react('👍').then(() => message.react('👎'));
-
-const filter = (reaction, user) => {
-	return ['👍', '👎'].includes(reaction.emoji.name) && user.id === message.author.id;
-};
-
-message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
-	.then(collected => {
-		const reaction = collected.first();
-
-		if (reaction.emoji.name === '👍') {
-			message.reply('you reacted with a thumbs up.');
-		} else {
-			message.reply('you reacted with a thumbs down.');
-		}
-	})
-	.catch(collected => {
-		message.reply('you reacted with neither a thumbs up, nor a thumbs down.');
-	});
+    kanaal.send(Acceptordecline).then(messageReaction => {
+        message.react('👍').then(() => message.react('👎'));
+    })
     
     module.exports.help = {
     }
