7c48125c82a9da2f78464d19f227dfdfb8f0f56f
abeata <62559783+abeata@users.noreply.github.com>
2020-03-25 14:43:34 +0100

    asd

--- commands/staffapply.js
+++ commands/staffapply.js
@@ -106,15 +106,4 @@ module.exports.run = async (bot, message, args) => {
             })
         })
     })
-
-    let embed = new Discord.MessageEmbed()
-        .setTitle('oof');
-
-    message.channel.send(embed).then(msg => {
-        msg.react(`🤔`)
-        msg.awaitReactions(`🤔`)
-    })
-}
-module.exports.help = {
-    name: "staffapply"
 }
--- commands/test.js
+++ commands/test.js
@@ -1,7 +1,13 @@
 const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+    const oof = new Discord.MessageEmbed()
+        .setTitle('oof');
 
+    message.channel.send(embed).then(msg => {
+        msg.react(`🤔`)
+        msg.awaitReactions(`🤔`)
+    })
 }
 module.exports.help = {
     name: "test"
