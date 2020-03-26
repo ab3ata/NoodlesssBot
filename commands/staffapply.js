3c82401e2e234a4985b34a37e3c8fe1562f70220
abeata <62559783+abeata@users.noreply.github.com>
2020-03-26 11:55:18 +0100

    Update staffapply.js

--- commands/staffapply.js
+++ commands/staffapply.js
@@ -1,5 +1,6 @@
 const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
+    const filter = m => m.author.id === message.author.id;
     mylist = [`testing`, `tesstterrr`];
     for (var i=0; i < mylist.length; i++) {
         message.channel.awaitMessages(filter, { max: 1 }).then(collected => {
