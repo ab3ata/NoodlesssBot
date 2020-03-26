b9ace741f56bd371857699745df9489d42935a7f
abeata <62559783+abeata@users.noreply.github.com>
2020-03-26 13:53:37 +0100

    Update staffapply.js

--- commands/staffapply.js
+++ commands/staffapply.js
@@ -3,17 +3,16 @@ module.exports.run = async (bot, message, args) => {
     const filter = m => m.author.id === message.author.id;
     mylist = [`Welcome`, `wait`, `how are you doing`, `when did you decided to ?`, `urg testmessages`];
     for (var i = 0; i < mylist.length; i++) {
-        message.channel.awaitMessages(filter, { max: 1 }).then(collected2 => {
+        message.channel.awaitMessages(filter, { max: 1 }).then(collected => {
             message.channel.send({
                 embed: {
                     color: 0xFFC300,
                     description: mylist[i]
-                }
-            })
-                .catch(collected2 => {
+                }})
+                .catch(collected => {
                     message.reply('you reacted with neither a thumbs up, nor a thumbs down.');
                 })
-        })
+                })
     }
 }
 module.exports.help = {
