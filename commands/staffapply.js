0248d3d73cc9644ce0cfac09cedc63bc93f00a55
abeata <62559783+abeata@users.noreply.github.com>
2020-03-26 13:50:38 +0100

    Update staffapply.js

--- commands/staffapply.js
+++ commands/staffapply.js
@@ -1,13 +1,19 @@
 const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     const filter = m => m.author.id === message.author.id;
-    mylist = [`Welcome`,  message.channel.awaitMessages(filter, { max: 1 }).then(collected2 => { `when??`}), `how are you doing`, `when did you decided to ?`, `urg testmessages`];
+    mylist = [`Welcome`, `wait`, `how are you doing`, `when did you decided to ?`, `urg testmessages`];
     for (var i = 0; i < mylist.length; i++) {
+        message.channel.awaitMessages(filter, { max: 1 }).then(collected2 => {
             message.channel.send({
                 embed: {
                     color: 0xFFC300,
                     description: mylist[i]
-                    }})
+                }
+            })
+                .catch(collected2 => {
+                    message.reply('you reacted with neither a thumbs up, nor a thumbs down.');
+                })
+        })
     }
 }
 module.exports.help = {
