f219955649a045f69fc1ca3f2c23cae63b32f899
abeata <62559783+abeata@users.noreply.github.com>
2020-03-27 08:26:32 +0100

    Update staffapply.js

--- commands/staffapply.js
+++ commands/staffapply.js
@@ -4,17 +4,21 @@ module.exports.run = async (bot, message, args) => {
     mylist = [`Welcome`, `test message`, `test message2`, `test message3`, `testmessages`];
     for (var i = 0; i < mylist.length; i++) {
         message.channel.send().then(msg => {
-            message.channel.awaitMessages(filter, { max: 1 }).then(collected2 => {
-            })
-                ({
+            message.channel.awaitMessages(filter, { max: 1 }).then(collected => {
+                message.channel.send({
                     embed: {
                         color: 0xFFC300,
                         description: mylist[i]
                     }
                 })
             })
-        }
-}
-    module.exports.help = {
-        name: "test"
+        })
     }
+            let username = collected.first().content
+            const applications = new discord.MessageEmbed()
+                .setColor('0xFFC300')
+                .setTitle(username);
+}
+module.exports.help = {
+    name: "test"
+}
