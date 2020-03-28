0f3372e3f84bd26f5fb23c3ef3cc4883cdee0327
abeata <62559783+abeata@users.noreply.github.com>
2020-03-28 13:52:05 +0100

    Update staffapply.js

--- commands/staffapply.js
+++ commands/staffapply.js
@@ -1,19 +1,16 @@
 const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     const filter = m => m.author.id === message.author.id;
-    mylist = [`Welcome`, `test message`, `test message2`, `test message3`, `testmessages`];
+    mylist = [`vraag 1`, `vraag 2`, `3de vraag`, `laatste vraag`];
+    await message.channel.send("welkom Je dient de volgende vragen te beantwoorden alvorens je verder kan.")
     for (var i = 0; i < mylist.length; i++) {
-        message.channel.send().then(msg => {
-            message.channel.awaitMessages(filter, { max: 1 }).then(collected => {
-                message.channel.send({
-                    embed: {
-                        color: 0xFFC300,
-                        description: mylist[i]
-                    }
-                })
+        await message.channel.send(mylist[i]).then(async msg => {
+            await message.channel.awaitMessages(filter, { max: 1 }).then(Collect => {
+               console.log(Collect.first().content);
             })
         })
     }
+    await message.channel.send("Dank je wel voor het invullen van onze vragen lijst");
 }
 module.exports.help = {
     name: "test"
