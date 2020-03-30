7fa7ef8aac26355955cb9ef2d5bc22c307423dc4
abeata <62559783+abeata@users.noreply.github.com>
2020-03-30 12:53:23 +0200

    Update staffapply.js

--- commands/staffapply.js
+++ commands/staffapply.js
@@ -2,9 +2,9 @@ const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     const filter = m => m.author.id === message.author.id;
     testList = [`vraag 1:`, `vraag 2:`, `vraag 4:`, `vraag 5:`];
-    let embed = new discord.MessageEmbed()
-    .setTitle(testList[i]);
     for (var i = 0; i < testList.length; i++) {
+        let embed = new discord.MessageEmbed()
+        .setTitle(testList[i]);
         await message.channel.send(embed).then(async msg => {
             await message.channel.awaitMessages(filter, { max: 1 }).then(Collect => {
                 console.log(Collect.first().content);
