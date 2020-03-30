98162bcc50b58651a914dbaed8fda4356155e63b
abeata <62559783+abeata@users.noreply.github.com>
2020-03-30 12:43:41 +0200

    Update staffapply.js

--- commands/staffapply.js
+++ commands/staffapply.js
@@ -3,14 +3,14 @@ module.exports.run = async (bot, message, args) => {
     const filter = m => m.author.id === message.author.id;
     testList = [`vraag 1:`, `vraag 2:`, `vraag 4:`, `vraag 5:`];
     for (var i = 0; i < testList.length; i++) {
-        await message.channel.send(testList[i]).then(async msg => {
+        await message.channel.send(embed).then(async msg => {
             await message.channel.awaitMessages(filter, { max: 1 }).then(Collect => {
                 console.log(Collect.first().content);
             })
         })
     }
-    var kanaal = message.guild.channels.cache.find(c => c.name == "application-log");
-    await kanaal.send(``);
+    let embed = new discord.MessageEmbed()
+            .setTitle(testList);
 
 }
 module.exports.help = {
