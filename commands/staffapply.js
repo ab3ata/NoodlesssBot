60ebdad56d9181b217e5589a398da148b5ed58ef
abeata <62559783+abeata@users.noreply.github.com>
2020-04-01 18:50:45 +0200

    Update staffapply.js

--- commands/staffapply.js
+++ commands/staffapply.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
             .setColor(`0xFFC300`);
         await message.channel.send(embed).then(async msg => {
             await message.channel.awaitMessages(filter, { max: 1 }).then(Collect => { 
-                while (i = 3) {
+                if(i = 3) {
                     message.channel.send(Collect);
                   }
             })
