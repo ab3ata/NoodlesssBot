e44ab8f920f042a17f5cbfa3bc8d1055ab161f21
abeata <62559783+abeata@users.noreply.github.com>
2020-04-07 19:53:21 +0200

    Update staffapply.js

--- commands/staffapply.js
+++ commands/staffapply.js
@@ -4,40 +4,40 @@ module.exports.run = async (bot, message, args) => {
   const questions = ["What is your name?", "What is your age?"];
 
   if (!args.length) {
-		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-	}
-	else if (args[0] === 'staff') {
-		async function* generateQuestions(message) {
+    return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+  }
+  else if (args[0] === 'staff') {
+    async function* generateQuestions(message) {
       for (const question of questions) {
         await message.channel.send(question);
-    
+
         yield message.channel
           .awaitMessages(filter, { max: 1 })
           .then(collected => collected.first() && collected.first().content);
       }
     }
-    
+
     const generator = generateQuestions(message);
     const answers = [];
-    
+
     for await (const answer of generator) {
       if (!answer) {
         return;
       }
-    
+
       answers.push(answer);
     }
-	}
-      let embed = new discord.MessageEmbed()
-        .setColor(`#0xFFC300`)
-        .setTitle(`new staff application!`)
-        .addField(
-          { name: questions[0], value: answers[0] },
-          { name: questions[1], value: answers[1] }
-        )
-      var kanaal = message.guild.channels.cache.find(c => c.name == "application-log");
-      kanaal.send(embed);
+    let embed = new discord.MessageEmbed()
+    .setColor(`#0xFFC300`)
+    .setTitle(`new staff application!`)
+    .addField(
+      { name: questions[0], value: answers[0] },
+      { name: questions[1], value: answers[1] }
+    )
+  var kanaal = message.guild.channels.cache.find(c => c.name == "application-log");
+  kanaal.send(embed);
   }
+}
 module.exports.help = {
   name: "apply"
 }
