aaecc9289ca7824a207ea3d2945e07a1592d8eef
abeata <62559783+abeata@users.noreply.github.com>
2020-04-07 15:03:12 +0200

    Update staffapply.js

--- commands/staffapply.js
+++ commands/staffapply.js
@@ -1,23 +1,29 @@
 const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-    const filter = m => m.author.id === message.author.id;
-    testList = [`question 1:`, `question 2:`, `question 4:`, `question 5:`];
-    if (!args.length) {
-        message.delete(1)
-        message.channel.send(`You didn't provide any arguments, ${message.author}!`).then(message.delete(5000));
-    }
-    else if (args[0] === 'staff') {
-        for (var i = 0; i < testList.length; i++) {
-            let embed = new discord.MessageEmbed()
-                .setTitle(testList[i])
-                .setColor(`0xFFC300`);
-            await message.channel.send(embed).then(async msg => {
-                await message.channel.awaitMessages(filter).then(Collect => {
-                    
-                })
-            })
-        }
-    }
+    const questions = ["What is your name?", "What is your age?"];
+
+async function* generateQuestions(message) {
+  for (const question of questions) {
+    await message.channel.send(question);
+
+    yield message.channel
+      .awaitMessages(filter, { max: 1, time: 1000 })
+      .then(collected => collected.first() && collected.first().content);
+  }
+}
+
+const generator = generateQuestions(message);
+const answers = [];
+
+for await (const answer of generator) {
+  if (!answer) {
+    return; // Got no answer
+  }
+
+  answers.push(answer);
+}
+
+console.log(answers);
 }
 module.exports.help = {
     name: "apply"
