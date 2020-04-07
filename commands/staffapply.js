7ea9d1dbacd7f536c80f8fd6784ba1163c0ca3c8
abeata <62559783+abeata@users.noreply.github.com>
2020-04-07 15:37:11 +0200

    Update staffapply.js

--- commands/staffapply.js
+++ commands/staffapply.js
@@ -1,29 +1,29 @@
 const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-    const questions = ["What is your name?", "What is your age?"];
+  const questions = ["What is your name?", "What is your age?"];
 
-async function* generateQuestions(message) {
-  for (const question of questions) {
-    await message.channel.send(question);
-
-    yield message.channel
-      .awaitMessages(filter, { max: 1, time: 1000 })
-      .then(collected => collected.first() && collected.first().content);
+  async function* generateQuestions(message) {
+    for (const question of questions) {
+      await message.channel.send(question);
+  
+      yield message.channel
+        .awaitMessages(filter, { max: 1, time: 1000 })
+        .then(collected => collected.first() && collected.first().content);
+    }
   }
-}
-
-const generator = generateQuestions(message);
-const answers = [];
-
-for await (const answer of generator) {
-  if (!answer) {
-    return; // Got no answer
+  
+  const generator = generateQuestions(message);
+  const answers = [];
+  
+  for await (const answer of generator) {
+    if (!answer) {
+      return; // Got no answer
+    }
+  
+    answers.push(answer);
   }
-
-  answers.push(answer);
-}
-
-console.log(answers);
+  
+  console.log(answers);
 }
 module.exports.help = {
     name: "apply"
