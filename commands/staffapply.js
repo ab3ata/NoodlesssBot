9fd93c8ac06b5b6035d8a1c240412d286150267e
abeata <62559783+abeata@users.noreply.github.com>
2020-04-07 16:38:00 +0200

    Update staffapply.js

--- commands/staffapply.js
+++ commands/staffapply.js
@@ -2,13 +2,13 @@ const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   if (!args.length) {
     message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-    message.channel.bulkDelete(19, true).catch(err => {
+    message.channel.bulkDelete(2, true).catch(err => {
       console.error(err);
       message.channel.send('there was an error trying to prune messages in this channel!');
     })
 
   }
-  else if (args[0] === 'staff') {
+  else if (args[1] === 'staff') {
     const filter = m => m.author.id === message.author.id;
     const questions = ["What is your name?", "What is your age?"];
     async function* generateQuestions(message) {
@@ -16,7 +16,7 @@ module.exports.run = async (bot, message, args) => {
         await message.channel.send(question);
 
         yield message.channel
-          .awaitMessages(filter, { max: 1, time: 1000 })
+          .awaitMessages(filter, { max: 1 })
           .then(collected => collected.first() && collected.first().content);
 
       }
