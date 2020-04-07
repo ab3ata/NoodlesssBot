19a4bb144f110d8e5124549352d82051294929d5
abeata <62559783+abeata@users.noreply.github.com>
2020-04-07 08:58:40 +0200

    Update staffapply.js

--- commands/staffapply.js
+++ commands/staffapply.js
@@ -9,12 +9,12 @@ module.exports.run = async (bot, message, args) => {
         await message.channel.send(embed).then(async msg => {
             await message.channel.awaitMessages(filter, { max: 1 }).then(Collect => {
                 let username = Collect.first().content
-                message.channel.awaitMessages(filter, { max: 1 }).then(Collect2 => {
-                    let username = Collect2.first().content
-                })
-                while (i === 3) {
+                while (i < 3) {
                     i++
                     message.channel.send(username);
+                    await message.channel.awaitMessages(filter, { max: 1 })
+                    message.edit(username);
+                    
                   }
             })
         })
