c918dac3678853541b3d5a9e01523b2979492272
abeata <62559783+abeata@users.noreply.github.com>
2020-04-07 10:19:25 +0200

    Update staffapply.js

--- commands/staffapply.js
+++ commands/staffapply.js
@@ -1,23 +1,32 @@
 const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     const filter = m => m.author.id === message.author.id;
-    testList = [`vraag 1:`, `vraag 2:`, `vraag 4:`, `vraag 5:`];
-    for (var i = 0; i < testList.length; i++) {
-        let embed = new discord.MessageEmbed()
-            .setTitle(testList[i])
-            .setColor(`0xFFC300`);
-        await message.channel.send(embed).then(async msg => {
-            await message.channel.awaitMessages(filter, { max: 1 }).then(Collect => {
-                Collect.array();
-                while (i === 3) {
-                    i++
-                    Array.from(Collect);
-                    message.channel.send(Collect.first(5));
-                    
-                    
-                  }
-            })
-        })
+    testList = [`question 1:`, `question 2:`, `question 4:`, `question 5:`];
+    if (command === 'apply') {
+        if (!args.length) {
+            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        }
+        else if (args[0] === 'staff') {
+            for (var i = 0; i < testList.length; i++) {
+                let embed = new discord.MessageEmbed()
+                    .setTitle(testList[i])
+                    .setColor(`0xFFC300`);
+                await message.channel.send(embed).then(async msg => {
+                    await message.channel.awaitMessages(filter, { max: 1 }).then(Collect => {
+                        Collect.array();
+                        while (i === 3) {
+                            i++
+                            Array.from(Collect);
+                            message.channel.send(Collect.first(5));
+                            
+                            
+                          }
+                    })
+                })
+            }
+        }
+
+        message.channel.send(`First argument: ${args[0]}`);
     }
 }
 module.exports.help = {
