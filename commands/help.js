77a3000bb2e8396731782b42c282ede8d7cfa775
abeata <62559783+abeata@users.noreply.github.com>
2020-04-08 09:22:30 +0200

    fg

---
+++ commands/help.js
@@ -0,0 +1,27 @@
+const discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+    let embed = new discord.MessageEmbed()
+    .setTitle(`Help`)
+    .setDescription(`this is a list of help commands:`)
+    .setColor(`#0xFFC300`)
+    .addFields(
+        { name: `!apply staff`, value: `do this command to apply for the staff position` },
+        { name: `!apply builder`, value: `do this command to apply for the builder position` },        
+    )
+    message.channel.send(embed);
+
+    if(member.roles.cache.has('role-id-here')){
+    let embed = new discord.MessageEmbed()
+    .setTitle(`Help`)
+    .setDescription(`this is a list of help commands:`)
+    .setColor(`#0xFFC300`)
+    .addFields(
+        { name: `!apply staff`, value: `do this command to apply for the staff position` },
+        { name: `!apply builder`, value: `do this command to apply for the builder position` },
+        { name: `!work`, value: `an command to see if it works?` },        
+    )
+    }
+}
+module.exports.help = {
+    name: "help"
+}
--- commands/test.js
+++ commands/test.js
@@ -1,14 +1,13 @@
 const discord = require("discord.js");
-
 module.exports.run = async (bot, message, args) => {
     let embed = new discord.MessageEmbed()
-        .setTitle('oof');
+.setTitle('oof');
 
-    message.channel.send(embed).then(msg => {
-        msg.react(`🤔`)
-        msg.awaitReactions(`🤔`)
-    })
+message.channel.send(embed).then(msg => {
+msg.react(`🤔`)
+msg.awaitReactions(`🤔`)
+})
 }
 module.exports.help = {
-    name: "stop"
+    name: "testingpurposecommand"
 }
