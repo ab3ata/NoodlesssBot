757964bd8956c491b504a915883c46d766137154
abeata <62559783+abeata@users.noreply.github.com>
2020-04-08 17:57:25 +0200

    sd

--- commands/staff-apply.js
+++ commands/staff-apply.js
@@ -51,6 +51,61 @@ module.exports.run = async (bot, message, args) => {
     var kanaal = message.guild.channels.cache.find(c => c.name == "application-log");
     kanaal.send(embed);
   }
+
+
+
+
+
+
+  const questionsbuilder = ["minecraft name:", "real name: (not needed)", "age:", "why do you want to be builder?"];
+  if (!args.length) {
+    let embed = new discord.MessageEmbed()
+    .setTitle(`Apply Help`)
+    .setDescription(`this is a list of help commands:`)
+    .setColor(`#0xFFC300`)
+    .addFields(
+        { name: `!apply staff`, value: `do this command to apply for the staff position` },
+        { name: `!apply builder`, value: `do this command to apply for the builder position` },        
+    )
+  }
+  else if (args[0] === 'builder') {
+    async function* generateQuestions(message) {
+      for (const question of questionsbuilder) {
+        let embed = new discord.MessageEmbed()
+          .setColor(`#0xFFC300`)
+          .setDescription(question)
+        await message.channel.send(embed);
+
+        yield message.channel
+          .awaitMessages(filter, { max: 1 })
+          .then(collected => collected.first() && collected.first().content);
+      }
+    }
+
+    const generator = generateQuestions(message);
+    const answers = [];
+
+    for await (const answer of generator) {
+      if (!answer) {
+        return;
+      }
+
+      answers.push(answer);
+    }
+    message.channel.bulkDelete(9, true);
+    let embed = new discord.MessageEmbed()
+      .setColor(`#0xFFC300`)
+      .setTitle(`New builder application`)
+      .addFields(
+        { name: questionsbuilder[0], value: answers[0] },
+        { name: questionsbuilder[1], value: answers[1] },
+        { name: questionsbuilder[2], value: answers[2] },
+        { name: questionsbuilder[3], value: answers[3] }
+      )
+      .setFooter(`applicant: ${messsage.author}`)
+    var kanaal = message.guild.channels.cache.find(c => c.name == "application-log");
+    kanaal.send(embed);
+  }
 }
 module.exports.help = {
   name: "apply"
