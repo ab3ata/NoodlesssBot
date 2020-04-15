f99740c2b624c11fc4bd8bc12d2b83eca0728cf5
abeata <62559783+abeata@users.noreply.github.com>
2020-04-15 16:43:12 +0200

    asd
    sad

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ bot.on("ready", async () => {
 
     console.log('LightMc bot is online!');
 
-    bot.user.setActivity("LightMc", { type: "PLAYING" });
+    bot.user.setActivity("NOODLESSS", { type: "PLAYING" });
 
 });
 
