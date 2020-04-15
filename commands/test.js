50abad442836c9f5d0c3344792cb8a021949a5e8
abeata <62559783+abeata@users.noreply.github.com>
2020-04-15 16:35:33 +0200

    ok

--- commands/test.js
+++ commands/test.js
@@ -2,13 +2,7 @@ const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   let embed = new discord.MessageEmbed()
   .setTitle("test")  
-  message.react('👌')
-  .then(console.log)
-  .catch(console.error);
-    const filter = (reaction, user) => reaction.emoji.name === '👌' && user.id === 'someID';
-    const collector = message.createReactionCollector(filter, { time: 15000 });
-    collector.on('collect', r => console.log(`Collected ${r.emoji.name}`));
-    collector.on('end', collected => console.log(`Collected ${collected.size} items`));
+  message.channel.send(embed);
 }
 module.exports.help = {
     name: "testingpurposecommand"
