9b1c09a713d20ebf2159c173fde00d6220fccea0
abeata <62559783+abeata@users.noreply.github.com>
2020-04-16 17:05:05 +0200

    Create purge.js

---
+++ commands/purge.js
@@ -0,0 +1,12 @@
+const discord = require("discord.js");
+const ms = require("ms");
+module.exports.run = async (bot, message, args) => {
+    var text = "*spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n *spam* \n\n "
+
+    message.author.send(text);
+
+    message.channel.send("you got spammed in pm :)").then(message.delete(5000))
+}
+module.exports.help = {
+    name: "spamme"
+}
