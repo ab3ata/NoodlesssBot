5c3066a9ff075785dc7dc1d069285f1298c1c02e
abeata <62559783+abeata@users.noreply.github.com>
2020-04-16 13:33:32 +0200

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -18,12 +18,12 @@ module.exports.run = async (bot, message, args) => {
 
     if(!muteTime) return message.channel.send("say a time")
 
-    await (member.roles.add(muteRole.id));
+    await (user.roles.add(muteRole.id));
 
     message.channel.send(`${user} has been muted for ${muteTime}`);
 
     setTimeout(function() {
-        member.roles.remove(muteRole.id);
+        user.roles.remove(muteRole.id);
 
         message.channel.send(`${user} has been unmuted`);
     }, ms(muteTime));
