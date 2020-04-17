f50c1211ba0da98495417aa83de3bf39208a089e
abeata <62559783+abeata@users.noreply.github.com>
2020-04-17 18:01:03 +0200

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,33 +1,33 @@
 const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-    async (client, message, args) => {
-        if (message.deletable) {
-            message.delete();
-        }
-        if (!message.member.roles.cache.some(role => role.name === '𝓒𝓸 𝓛𝓮𝓪𝓭𝓮𝓻','𝓛𝓮𝓪𝓭𝓮𝓻')) {
-            return message.reply("You can't delete messages....").then(m => m.delete(5000));
-        }
+    if (!message.member.roles.cache.some(role => role.name === '𝓒𝓸 𝓛𝓮𝓪𝓭𝓮𝓻','𝓛𝓮𝓪𝓭𝓮𝓻')) {message.channel.send("sorry you do not have permission to do this command")};
+
+    if(!args[0]) return message.channel.send("give a number!");
+
+    if(Number.isInteger(parseInt(args[0]))){
+
+        var amount = parseInt(args[0]) + 1;
+
+        message.channel.bulkDelete(amount).then(() => {
+
+            if(args[0] == 0){
+                message.channel.send(`i cant delete 0 messages you dumb`).then(msg => msg.delete(5000))
+            }else if(args[0] == 1){
+                message.channel.send(`i deleted ${amount} message`).then(msg => msg.delete(5000))
+            }else{
+                message.channel.send(`i deleted ${amount} messages`).then(msg => msg.delete(5000))
+            }
+         });
 
 
-        if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
-            return message.reply("Yeah.... That's not a numer? I also can't delete 0 messages by the way.").then(m => m.delete(5000));
-        }
-        if (!message.member.roles.cache.some(role => role.name === '𝓒𝓸 𝓛𝓮𝓪𝓭𝓮𝓻','𝓛𝓮𝓪𝓭𝓮𝓻')) {
-            return message.reply("Sorryy... I can't delete messages.").then(m => m.delete(5000));
-        }
+    }else{
+        return message.channel.send("only numbers allowed!")
 
-        let deleteAmount;
-
-        if (parseInt(args[0]) > 100) {
-            deleteAmount = 100;
-        } else {
-            deleteAmount = parseInt(args[0]);
-        }
-
-        message.channel.bulkDelete(deleteAmount, true)
-            .then(deleted => message.channel.send(`I deleted \`${deleted.size}\` messages.`))
-            .catch(err => message.reply(`Something went wrong... ${err}`));
     }
+
+
+
+
 }
 module.exports.help = {
     name: "purge"
