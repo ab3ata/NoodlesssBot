80f1a4e02286267abe2653d82d73c117879f777c
abeata <62559783+abeata@users.noreply.github.com>
2020-04-17 15:47:06 +0200

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -2,12 +2,12 @@ const discord = require("discord.js");
 const ms = require("ms");
 module.exports.run = async (bot, message, args) => {
     
-    if (!message.member.roles.cache.some(role => role.name === '𝓒𝓸 𝓛𝓮𝓪𝓭𝓮𝓻', '𝓛𝓮𝓪𝓭𝓮𝓻')) {message.channel.send("sorry you do not have permission to do this command");}
+    if (!message.member.roles.cache.some(role => role.name === '𝓒𝓸 𝓛𝓮𝓪𝓭𝓮𝓻','𝓛𝓮𝓪𝓭𝓮𝓻')) {message.channel.send("sorry you do not have permission to do this command");}
     var user = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
 
     if (!user) return message.channel.send("please say a valid username");
 
-    if (message.member.roles.cache.some(role => role.name === '𝓒𝓸 𝓛𝓮𝓪𝓭𝓮𝓻', '𝓛𝓮𝓪𝓭𝓮𝓻')){
+    if (message.member.roles.cache.some(role => role.name === '𝓒𝓸 𝓛𝓮𝓪𝓭𝓮𝓻','𝓛𝓮𝓪𝓭𝓮𝓻')){
 
     var muteRole = message.guild.roles.cache.find(role => role.name === "muted");
 
@@ -19,7 +19,7 @@ module.exports.run = async (bot, message, args) => {
 
     await (user.roles.add(muteRole.id));
 
-    message.channel.send(`${user} has been muted for ${muteTime}`);
+    message.channel.send(`${user} has been muted for ${muteTime}`).then(message.delete(5000))
 
     setTimeout(function() {
         user.roles.remove(muteRole.id);
