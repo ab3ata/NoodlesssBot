3a63053db07d9443f276024d1989450cf4c56a11
abeata <abeataonline@gmail.com>
2020-06-06 11:16:53 +0000

    fff

--- commands/talking.js
+++
@@ -1,9 +0,0 @@
-const discord = require("discord.js");
-module.exports.run = async (bot, message, args) => {
-let embed = new discord.MessageEmbed()
-  .setTitle("hi how are you today?")  
-  message.channel.send(embed);
-}
-module.exports.help = {
-    name: "hello"
-}
