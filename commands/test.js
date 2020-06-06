b2c14fd24e1b0e5967b67c2b091dbdb0d292b2aa
abeata <62559783+abeata@users.noreply.github.com>
2020-06-06 12:43:22 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,7 +1,7 @@
 const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   let embed = new discord.MessageEmbed()
-  .setTitle("hi how are you today?")  
+  .setTitle("hiiiii how are you today?")  
   message.channel.send(embed);
 }
 module.exports.help = {
